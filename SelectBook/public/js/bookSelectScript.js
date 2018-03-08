f34d9b58ee0fb7f78a90035bdf0ceeabc023a764
perplexedotter <panda3125@gmail.com>
2018-03-07 20:43:12 -0500

    Book Selection Script

--- SelectBook/public/js/bookSelectScript.js
+++ SelectBook/public/js/bookSelectScript.js
@@ -1,2 +1,3 @@
 //This will contain the script that will assign event handlers to the book icons which will send a 
 //get request to the server with the chosen books ID
+function selectBook(bookID){ window.location.assign("/bookSelect/" + bookID);}
